f240b263f4fb916d9701b8b0c1ab8d8884416c8a
njim3 <njim3@njim3deiMac.(none)>
2018-07-07 22:59:49 +0800

    Some easy problems solution

---
+++ LeetcodeDemo/main.c
@@ -0,0 +1,181 @@
+//
+//  main.c
+//  LeetcodeDemo
+//
+//  Created by njim3 on 2018/7/7.
+//
+
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+/*
+ * 7. Reverse Integer
+ */
+int reverse(int x) {
+    int negtive = 0;
+    
+    // 判断正负数
+    if (x == 0)
+        return 0;
+    else if (x < 0) {
+        negtive = 1;
+        x = -x;
+    }
+    
+    int reverseNum = 0;
+    
+    do {
+        int temp = reverseNum;
+        reverseNum = reverseNum * 10 + x % 10;
+        
+        if (((reverseNum - x % 10) / 10) != temp)   // 溢出
+            return 0;
+        
+        
+        x /= 10;
+        
+    } while (x != 0);
+    
+    return negtive == 1 ? -reverseNum : reverseNum;
+}
+
+/*
+ * 9. Palindrome Number
+ */
+bool isPalindrome(int x) {
+    if (x < 0)
+        return false;
+    
+    int reverseNum = 0, xcopy = x;
+    
+    do {
+        int temp = reverseNum;
+        
+        reverseNum = reverseNum * 10 + x % 10;
+        if ((reverseNum - x % 10) / 10 != temp)
+            return false;
+        
+        x /= 10;
+    } while (x != 0);
+    
+    if (reverseNum == xcopy)
+        return true;
+    
+    return false;
+}
+
+/*
+ * 13. Roman to Integer
+ */
+
+int romanVal(char ch) {
+    switch (ch) {
+        case 'I':
+            return 1;
+        case 'V':
+            return 5;
+        case 'X':
+            return 10;
+        case 'L':
+            return 50;
+        case 'C':
+            return 100;
+        case 'D':
+            return 500;
+        case 'M':
+            return 1000;
+            
+        default:
+            return 0;
+    }
+    
+    return 0;
+}
+
+int romanToInt(char* s) {
+    unsigned long strLen = strlen(s);
+    
+    if (s == NULL || strLen == 0)
+        return 0;
+    
+    int val = 0;
+    
+    for (int i = 0; i < strLen - 1; ++i) {
+        int curChVal = romanVal(s[i]);
+        int nextChVal = romanVal(s[i + 1]);
+        
+        if (curChVal < nextChVal) {
+            val -= curChVal;
+        } else
+            val += curChVal;
+    }
+    
+    val += romanVal(s[strLen - 1]);
+    
+    
+    return val;
+}
+
+/*
+ * 14. Longest Common Prefix
+ */
+char* longestCommonPrefix(char** strs, int strsSize) {
+    if (strsSize == 0)
+        return "";
+    
+    if (strsSize == 1)
+        return strs[0];
+    
+    long minStrIndex = 0, minStrSize = strlen(strs[0]);
+    
+    for (int i = 1; i < strsSize; ++i) {
+        unsigned long curStrLen = strlen(strs[i]);
+        
+        if (curStrLen == 0)
+            return "";
+        
+        if (curStrLen < minStrSize) {
+            minStrSize = curStrLen;
+            
+            minStrIndex = i;
+        }
+    }
+    
+    long stopIndex = minStrSize;
+    for (int i = 0; i < minStrSize; ++i) {
+        
+        char zeroStrCurCh = strs[0][i];
+        bool curLoopRes = true;
+        
+        for (int j = 1; j < strsSize; ++j) {
+            if (zeroStrCurCh != strs[j][i])
+                curLoopRes = false;
+        }
+        
+        if (curLoopRes == false) {
+            stopIndex = i;
+            
+            break;
+        }
+    }
+    
+    if (stopIndex == -1 || stopIndex == 0)
+        return "";
+    
+    char* resStr = (char*)malloc(sizeof(char) * (stopIndex + 1));
+    resStr[stopIndex] = '\0';
+    
+    strncpy(resStr, strs[minStrIndex], stopIndex);
+    
+    return resStr;
+}
+
+int main(int argc, char* argv[]) {
+    char* strs[] = {"c", "c"};
+    printf("%s\n", longestCommonPrefix(strs, 2));
+    
+    
+    return 0;
+}
